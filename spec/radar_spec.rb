eaa4602a5bbbf7a1008fa503827bf1868a45e144
Marcel de Graaf <mail@marceldegraaf.net>
2017-12-16 00:36:00 +0100

    add truthiness test

--- spec/radar_spec.rb
+++ spec/radar_spec.rb
@@ -29,6 +29,10 @@ RSpec.describe Radar, ".status_for" do
     it "has weather" do
       expect(status.weather).to eq("Overcast")
     end
+
+    it "tests the truth" do
+      expect(1 + 1).to eq(2)
+    end
   end
 
   context "when the airport cannot be found" do
